5ffab3e7505bd6adb74b8ba90c8db71ce1b9f2c1
danielljeon <48095779+danielljeon@users.noreply.github.com>
2025-03-23 13:56:12 -0400

    Add improved ADS114S08 driver

--- Core/Inc/ads114s08_hal_spi.h
+++ Core/Inc/ads114s08_hal_spi.h
@@ -35,39 +35,6 @@ extern uint16_t channel_data[9]; // Final readings per channel.
 /** Public functions. *********************************************************/
 
 /**
- * @brief Reads a single register from the ADS114S08 ADC.
- *
- * @param address The register address to be read.
- * @param rx_buffer The RX data byte buffer.
- * @param rx_length The number of bytes to be read.
- *
- * This function constructs the read register command by combining the
- * ADS114S08_CMD_RREG command with the register address and then retrieves
- * the value from the ADC.
- */
-void ads114s08_read_register(uint8_t address, uint8_t *rx_buffer,
-                             uint8_t rx_length);
-
-/**
- * @brief Writes a command to the ADS114S08.
- *
- * @param cmd The command byte to send.
- */
-void ads114s08_write_command(uint8_t cmd);
-
-/**
- * @brief Writes a value to a register in the ADS114S08.
- *
- * @param address The register address to write to.
- * @param value The value to be written.
- *
- * This function constructs the write register command by combining the
- * ADS114S08_CMD_WREG command with the register address and then sends the
- * value to be written.
- */
-void ads114s08_write_register(uint8_t address, uint8_t value);
-
-/**
  * @brief Initializes the ADS114S08 ADC.
  */
 void ads114s08_init(void);
